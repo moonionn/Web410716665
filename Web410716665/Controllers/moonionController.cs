6007dd8e24e55e5aa5fd9a10ff5330ef39ee3bcb
沈宜君 <sisisibibi@gmail.com>
2021-04-29 20:26:02 +0800

    so

---
+++ Web410716665/Controllers/moonionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410716665.Controllers
+{
+    public class moonionController : Controller
+    {
+        // GET: moonion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
